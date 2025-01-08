eba7f7ef854cf377a565370b15b21de0b4b248f3
Shriyan10 <shriyanbuzz@gmail.com>
2025-01-08 13:43:50 +0545

    feat: router for book details added

--- src/controller/BookDetailController.php
+++ src/controller/BookDetailController.php
@@ -1,34 +1,150 @@
 <?php
-require 'vendor/autoload.php';
+
+namespace App\controller;
+
 
 use App\Db\Database;
 use App\mapper\impl\BookDetailMapper;
+use App\model\BookDetail;
+use Exception;
+use Latte\Engine;
 
-$latte = new Latte\Engine;
-$database = new Database();
+class BookDetailController
+{
+    private Engine $latte;
+
+    public function __construct(Engine $latte)
+    {
+        $this->latte = $latte;
+    }
 
 
-$bookDetails = $database -> queryAll("SELECT * FROM book_details",  new BookDetailMapper());
+    function getAllBookDetails(): void
+    {
+        $database = new Database();
+        $query = "SELECT * FROM book_details";
 
-$params = [
-    'bookDetails' => $bookDetails
-];
+        $bookDetails = $database->queryAll($query, new BookDetailMapper());
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $params = [
+            'bookDetails' => $bookDetails,
+        ];
 
-    try {
-        $type = $_POST['type'];
-        if(strcmp("DELETE", $type) == 0){
-            $database->query("DELETE FROM book_details where id=%d", [$_POST['bookId']]);
-            header("Location: http://localhost/bookshop/BookListController.php");
-        }elseif(strcmp("EDIT", $type) == 0){
+        $this->latte->render('templates\books\book_list.latte', $params);
+    }
 
-            header("Location: http://localhost/bookshop/EditBookDetailController.php?bookId=" . $_POST['bookId']);
+    function getBookDetails(int $bookId): void
+    {
+        $database = new Database();
+
+        $query = "SELECT * FROM book_details WHERE id=" . $bookId;
+        $bookDetail = $database->queryOne($query, new BookDetailMapper());
+        $bookDetails = $database->queryAll("SELECT * FROM book_details", new BookDetailMapper());
+
+        $params = [
+            'bookDetail' => $bookDetail,
+        ];
+
+        // render to output
+        $this->latte->render('templates\users\edit_book.latte', $params);
+    }
+
+
+    function updateBookDetails(int $bookId): void
+    {
+        try {
+            $bookDetail = new BookDetail(
+                $bookId,
+                $_POST['title'] ?? null,
+                $_POST['author'] ?? null,
+                $_POST['publisher'] ?? null,
+                $_POST['isbn'] ?? null,
+                $_POST['price'] ?? null,
+                $_POST['imageUrl'] ?? null
+            );
+            $database = new Database();
+            $result = $database->query(
+                "UPDATE book_details SET title='%s', image_url='%s', author='%s', publisher='%s', isbn='%s', price=%d where id=%d",
+                [
+                    $bookDetail->getTitle(),
+                    $bookDetail->getImageUrl(),
+                    $bookDetail->getAuthor(),
+                    $bookDetail->getPublisher(),
+                    $bookDetail->getIsbn(),
+                    $bookDetail->getPrice(),
+                    $bookDetail->getId()
+                ],
+            );
+            if ($result) {
+                header("Location: http://localhost/bookshop/BookListController.php");
+            }
+        } catch (Exception $e) {
+            var_dump($e);
+            header("Location: http://localhost/bookshop/404");
         }
-    } catch (Exception $e) {
-        var_dump($e);
+    }
+
+
+    function deleteBookDetails(int $bookId): void
+    {
+        try {
+            $database = new Database();
+            $result = $database->query("DELETE FROM book_details where id=%d", [$bookId]);
+            if ($result) {
+                header("Location: http://localhost/bookshop/books/");
+            }
+        } catch (Exception $e) {
+            var_dump($e);
+            header("Location: http://localhost/bookshop/404");
+        }
+    }
+
+    function saveBookDetailsPage(): void
+    {
+        $database = new Database();
+
+        $bookDetails = $database->queryAll("SELECT * FROM book_details", new BookDetailMapper());
+
+        $params = [
+            'bookDetails' => $bookDetails
+        ];
+
+        // render to output
+        $this->latte->render('templates\books\add_book.latte', $params);
+    }
+
+
+    function saveBookDetails(): void
+    {
+        try {
+            $database = new Database();
+            $bookDetail = new BookDetail(
+                null,
+                $_POST['title'] ?? null,
+                $_POST['author'] ?? null,
+                $_POST['publisher'] ?? null,
+                $_POST['isbn'] ?? null,
+                $_POST['price'] ?? null,
+                $_POST['imageUrl'] ?? null
+            );
+
+            $result = $database->query(
+                "INSERT INTO book_details(title, image_url, author, publisher, isbn, price) VALUES('%s','%s','%s','%s', '%s', %d)",
+                [
+                    $bookDetail->getTitle(),
+                    $bookDetail->getImageUrl(),
+                    $bookDetail->getAuthor(),
+                    $bookDetail->getPublisher(),
+                    $bookDetail->getIsbn(),
+                    $bookDetail->getPrice()
+                ],
+            );
+            if ($result) {
+                header("Location: http://localhost/bookshop/books/");
+            }
+        } catch (Exception $e) {
+            var_dump($e);
+            header("Location: http://localhost/bookshop/404");
+        }
     }
 }
-
-// render to output
-$latte->render('templates\books\book_list.latte', $params);
--- src/route/Router.php
+++ src/route/Router.php
@@ -3,6 +3,7 @@
 namespace App\route;
 
 
+use App\controller\BookDetailController;
 use App\controller\RoleController;
 use App\controller\UserController;
 use Latte\Engine;
@@ -31,7 +32,12 @@ class Router
 
         // users
         elseif (str_contains($path, 'bookshop/users/')) {
-            $this -> user($path);
+            $this->user($path);
+        }
+
+        //books
+        elseif (str_contains($path, 'bookshop/books/')) {
+            $this->book($path);
         }
 
         // 404 page
@@ -67,7 +73,7 @@ class Router
     function user(string $path): void
     {
         $userController = new UserController($this->latte);
-    if ($this->endsWith($path, 'users/')) {
+    if ($this->endsWith($path, '/users/')) {
         $userController->getAllUsers();
     }else if ($this->endsWith($path, '/save')) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -85,6 +91,30 @@ class Router
     } else if (str_contains($path, '/delete?userId=')) {
         $userController->deleteUser($_GET['userId']);
     }
+    }
+
+
+    function book(string $path): void
+    {
+        $bookDetailController = new BookDetailController($this->latte);
+        if ($this->endsWith($path, '/books/')) {
+            $bookDetailController->getAllBookDetails();
+        }else if ($this->endsWith($path, '/save')) {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $bookDetailController->saveBookDetails();
+            } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                $bookDetailController->saveBookDetailsPage();
+            }
+        }
+        else if (str_contains($path, '/edit?bookId=')) {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $bookDetailController->updateBookDetails($_GET['bookId']);
+            } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                $bookDetailController->getBookDetails($_GET['bookId']);
+            }
+        } else if (str_contains($path, '/delete?bookId=')) {
+            $bookDetailController->deleteBookDetails($_GET['bookId']);
+        }
     }
 
 
