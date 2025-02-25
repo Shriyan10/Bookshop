0115efba924ff9d0a1972452915fa56a8a8ba1f8
Shriyan10 <shriyanbuzz@gmail.com>
2025-02-25 19:21:24 +0545

    feat:refactored

--- src/controller/customer/CheckoutController.php
+++ src/controller/customer/CheckoutController.php
@@ -21,59 +21,72 @@ class CheckoutController extends BaseController
 
     function checkout(): void
     {
+        try {
+            $auth = new AuthenticationController($this->latte, $this->database);
 
-        $auth = new AuthenticationController($this->latte, $this->database);
+            if (!$auth->isLoggedIn()) {
+                $this->redirect("login");
+                return;
+            }
 
-        if(!$auth->isLoggedIn()){
-            $this->redirect("login");
+            if (!isset($_SESSION['cart'])) {
+                $this->redirect();
+                return;
+            }
+
+            $cart = $_SESSION['cart'];
+
+            $cartDetails = [];
+            $grandTotal = 0;
+
+            foreach ($cart as $productDetailId => $quantity) {
+
+                $cartDetail = new CartDetail();
+                $cartDetail->setQuantity($quantity);
+                $cartDetail->setId($productDetailId);
+
+                $bookDetail = $this->database->queryOne("SELECT * FROM product_details WHERE id=$productDetailId", new ProductDetailMapper());
+                $title = $bookDetail->title;
+                $cartDetail->setTitle($title);
+                $totalAmount = $bookDetail->price * $quantity;
+                $grandTotal += $totalAmount;
+                $cartDetail->setTotalAmount($totalAmount);
+                array_push($cartDetails, $cartDetail);
+            }
+
+            $products = [];
+            foreach ($cartDetails as $cartDetail) {
+                $specificProducts = $this->database->queryAll("SELECT * FROM products WHERE product_detail_id={$cartDetail->getId()} AND status='AVAILABLE' LIMIT {$cartDetail->getQuantity()}", new ProductMapper());
+
+                foreach ($specificProducts as $specificProduct) {
+                    array_push($products, $specificProduct);
+                }
+            }
+
+            $this->updateProductInventory($products, $grandTotal);
+
+            if (isset($_SESSION['cart'])) {
+                $_SESSION['cart'] = [];
+            }
+
+            $this->redirect("payments");
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            $this->redirect("500");
         }
+    }
 
-
-        $cart = $_SESSION['cart'];
-
-        $cartDetails = [];
-        $grandTotal = 0;
-        foreach ($cart as $productDetailId => $quantity) {
-
-            $cartDetail = new CartDetail();
-            $cartDetail->setQuantity($quantity);
-            $cartDetail->setId($productDetailId);
-
-            $bookDetail = $this->database->queryOne("SELECT * FROM product_details WHERE id=$productDetailId", new ProductDetailMapper());
-            $title = $bookDetail->title;
-            $cartDetail->setTitle($title);
-            $totalAmount = $bookDetail->price*$quantity;
-            $grandTotal += $totalAmount;
-            $cartDetail->setTotalAmount($totalAmount);
-            array_push($cartDetails, $cartDetail);
-        }
-
-        $products = [];
-        foreach ($cartDetails as $cartDetail) {
-          $specificProducts =   $this -> database ->queryAll("SELECT * FROM products WHERE product_detail_id={$cartDetail->getId()} AND status='AVAILABLE' LIMIT {$cartDetail->getQuantity()}", new ProductMapper());
-
-          foreach ($specificProducts as $specificProduct) {
-              array_push($products, $specificProduct);
-          }
-        }
-
+    public function updateProductInventory(array $products, float|int $grandTotal): void
+    {
         foreach ($products as $product) {
-            $sql = "UPDATE products SET status='SOLD' WHERE id=%d";
-            $result = $this->database->query($sql, [$product->getId()]);
+            $this->database->query("UPDATE products SET status='SOLD' WHERE id=%d", [$product->getId()]);
         }
 
         $paymentId = $this->database->queryWithId("INSERT into payments(total_cost,user_id) VALUES(%d,%d)", [$grandTotal, $_SESSION['user']->getId()]);
 
         foreach ($products as $product) {
-            $sql = "INSERT INTO payment_details(product_id, payment_id) VALUES(%d,%d)";
-            $result = $this->database->query($sql, [$product->getId(), $paymentId]);
+            $this->database->query("INSERT INTO payment_details(product_id, payment_id) VALUES(%d,%d)", [$product->getId(), $paymentId]);
         }
-        if (isset($_SESSION['cart'])) {
-            // Create a cart if it does not exist
-            $_SESSION['cart'] = [];
-        }
-
-
-        $this->redirect();
     }
 }
